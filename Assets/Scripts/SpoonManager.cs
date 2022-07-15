16a06ab7253c2a69572d414ff626fd8cc116c68f
Filipe Coelho <lipe.msc@gmail.com>
2022-07-15 21:28:55 +0100

    Now the keyboard makes the "spoon" rotate.
    We can now observe the physics engine working well.
    Next need to figure out how to make this rotate with the phone's gyroscope.

--- Assets/Scripts/SpoonManager.cs
+++ Assets/Scripts/SpoonManager.cs
@@ -4,20 +4,33 @@ using UnityEngine;
 
 public class SpoonManager : MonoBehaviour {
     public Rigidbody spoonrb;
+    private Vector3 angleVelocity;
+
+    void RotateRB(Rigidbody rb, Vector3 vector) {
+        Quaternion deltaRotation = Quaternion.Euler(vector * Time.fixedDeltaTime);
+        rb.MoveRotation(spoonrb.rotation * deltaRotation);
+    }
+
     // Start is called before the first frame update
     void Start() {
-        
+        Debug.Log(SystemInfo.supportsGyroscope);
     }
 
     // Update is called once per frame
     void FixedUpdate() {
-        if(Input.GetKey("up")) {
-            spoonrb.velocity = Vector3.up * 3f;
-        } else if(Input.GetKey("down")) {
-            spoonrb.velocity = Vector3.down * 3f;
+         if(Input.GetKey("up")) {
+            RotateRB(spoonrb, new Vector3(50, 0, 0));
+        } if(Input.GetKey("down")) {
+            RotateRB(spoonrb, new Vector3(-50, 0, 0));
+        } if(Input.GetKey("left")) {
+            RotateRB(spoonrb, new Vector3(0, 0, 50));
+        } if(Input.GetKey("right")) {
+            RotateRB(spoonrb, new Vector3(0, 0, -50));
         } else {
             spoonrb.velocity = new Vector3(0, 0, 0);
         }
         Debug.Log(spoonrb.velocity);
+
+        
     }
 }
