d9020280d12ea3dd83cc7611f4d84ee76f083854
Filipe Coelho <lipe.msc@gmail.com>
2022-07-16 16:57:49 +0100

    Managed to get info out of the gyroscope.
    The code now sends gyro information to the Debug Log. This cannot be
    accessed in the usual manner.
    Instead, one has to build the project to Android, enable ADB on
    Android's Developer Options, install adb on the computer and any
    necessary drivers, install the build to the device and then run "adb
    logcat" (the pros that use bash or a similar unix shell can run "adb
    logcat | grep Unity" :hand_over_mouth:)
    
    O código agora envia informação do giroscópio para o Debug Log. Este não
    pode ser acedido da forma tradicional.
    Ao invés, tem de se fazer uma build do projeto para Android, ativar ADB
    nas Opções de Programador do Android, instalar adb no computador e
    quaisquer drivers necessários, instalar a build para o dispositivo e
    executar "adb logcat" (os pros que usam bash ou uma shell de unix
    semelhante podem executar "adb logcat | grep Unity" :hand_over_mouth:)

--- Assets/Scripts/SpoonManager.cs
+++ Assets/Scripts/SpoonManager.cs
@@ -14,9 +14,14 @@ public class SpoonManager : MonoBehaviour {
     // Start is called before the first frame update
     void Start() {
         Debug.Log(SystemInfo.supportsGyroscope);
+        Input.gyro.enabled = true;
     }
 
     // Update is called once per frame
+    void Update() {
+        Debug.Log(Input.gyro.attitude);
+    }
+    
     void FixedUpdate() {
          if(Input.GetKey("up")) {
             RotateRB(spoonrb, new Vector3(50, 0, 0));
@@ -29,8 +34,6 @@ public class SpoonManager : MonoBehaviour {
         } else {
             spoonrb.velocity = new Vector3(0, 0, 0);
         }
-        Debug.Log(spoonrb.velocity);
-
-        
     }
+    
 }
