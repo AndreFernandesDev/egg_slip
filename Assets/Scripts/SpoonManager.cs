70096c53d9870f1b786165e948f2a65ac568fa1e
Filipe Coelho <lipe.msc@gmail.com>
2022-07-19 01:14:46 +0100

    Sucessfully made the spoon move according to the Gyro.
    This is a major accomplishment in this project.
    We can now balance the egg in the spoon without needing the keyboard.
    All done with the device's gyroscope.

--- Assets/Scripts/SpoonManager.cs
+++ Assets/Scripts/SpoonManager.cs
@@ -8,32 +8,52 @@ public class SpoonManager : MonoBehaviour {
 
     void RotateRB(Rigidbody rb, Vector3 vector) {
         Quaternion deltaRotation = Quaternion.Euler(vector * Time.fixedDeltaTime);
-        rb.MoveRotation(spoonrb.rotation * deltaRotation);
+        var rotation = rb.rotation * deltaRotation;
+        //Debug.Log(rotation);
+        rb.MoveRotation(rotation);
+    }
+
+    void RotateRBTo(Rigidbody rb, Quaternion q) {
+        rb.MoveRotation(Quaternion.RotateTowards(rb.rotation, q, 10));
+    }
+
+    void RotateRBToFromVector(Rigidbody rb, Vector3 vector) {
+        RotateRBTo(rb, Quaternion.Euler(vector));
     }
 
     // Start is called before the first frame update
     void Start() {
         Debug.Log(SystemInfo.supportsGyroscope);
         Input.gyro.enabled = true;
+        Screen.autorotateToLandscapeLeft = false;
+        Screen.autorotateToLandscapeRight = false;
+        Screen.autorotateToPortraitUpsideDown = false;
+        Screen.autorotateToPortrait = true;
     }
 
     // Update is called once per frame
     void Update() {
+        /*spoonrb.velocity = Vector3.zero;
+        spoonrb.angularVelocity = Vector3.zero;*/
         Debug.Log(Input.gyro.attitude);
     }
     
     void FixedUpdate() {
-         if(Input.GetKey("up")) {
-            RotateRB(spoonrb, new Vector3(50, 0, 0));
+        Vector3 vec = Input.gyro.attitude.eulerAngles;
+        RotateRBToFromVector(spoonrb, new Vector3(-vec.x, 0, -vec.y));
+        if(Input.GetKey("up")) {
+            RotateRB(spoonrb, new Vector3(150, 0, 0));
         } if(Input.GetKey("down")) {
-            RotateRB(spoonrb, new Vector3(-50, 0, 0));
+            RotateRB(spoonrb, new Vector3(-150, 0, 0));
         } if(Input.GetKey("left")) {
-            RotateRB(spoonrb, new Vector3(0, 0, 50));
+            RotateRB(spoonrb, new Vector3(0, 0, 150));
         } if(Input.GetKey("right")) {
-            RotateRB(spoonrb, new Vector3(0, 0, -50));
-        } else {
-            spoonrb.velocity = new Vector3(0, 0, 0);
-        }
+            RotateRB(spoonrb, new Vector3(0, 0, -150));
+        } if (Input.GetKey("w")) {
+            RotateRBToFromVector(spoonrb, new Vector3(10,10,0));
+            //spoonrb.MoveRotation(new Quaternion(10,10,10,0).normalized);
+        } 
+        
     }
     
 }
